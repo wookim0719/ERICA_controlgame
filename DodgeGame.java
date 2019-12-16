460866a4cf32d6ffa2ddde57def2140617a85066
wookim0719 <ujbae@naver.com>
2019-12-16 22:48:12 +0900

    Mainclass update

--- DodgeGame.java
+++ DodgeGame.java
@@ -3,6 +3,7 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 public class DodgeGame {
 	public static void main(String[] args) {
@@ -10,12 +11,20 @@ public class DodgeGame {
 	}
 
 
-static  class GameFrame extends Frame{
+static  class GameFrame extends Frame implements Runnable{
 	 public static final int Width = 400, Height = 400;
-	 
+	 private final int INITIAL_ATTACKERS = 200;
+	 private final long DT = 20;
+	 private final double AVOIDER_SPEED = 4, ATTACK_SPEED =2;
+	 private ArrayList<Attacker> attackers;
+	 private ArrayList<Character> polots, deads;
 	 public GameFrame(){
 		 super("2019009870 김정우");
 		 initFrame();
+	}
+	public DodgeFrame(){
+		super("Our Own Dodge");
+		initFrame();
 	}
 
 
@@ -35,4 +44,12 @@ static  class GameFrame extends Frame{
 		g.fillRect(0,0,Width,Height);
 	}
  }
+	public void addAvoiderPilot(AvoiderPilot ap){
+		Character a = new Character(Width/2, Height/2);
+		a.setSpeed(AVOIDER_SPEED);
+		ap.setAvoider(a);
+		ap.setAttackers(attackers);
+		pilots.add(ap);
+		
+}
 }
